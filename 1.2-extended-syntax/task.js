edbdc7956e333f8bb3a72f443ed0ae2824e6b33b
Murat Kurbanov <glukymur@gmail.com>
2020-06-13 12:36:20 +0300

    I did this tasks

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,40 @@
+"use strict"
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let discriminant = b**2 - 4 * a * c;
+    let x = [];
+    
+    if(discriminant == 0){
+        x[0] = -b/(2*a);
+    }
+    else if(discriminant > 0){
+        x[0] = (-b + Math.sqrt(discriminant))/(2 * a);
+        x[1] = (-b - Math.sqrt(discriminant))/(2 * a);
+    }
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sum = 0;
+    let averageMark = 0;
+    if (marks.length == 0){
+        return 0;
+    } else if (marks.length > 5){
+        console.log('Оценок слишком много, они не будут учитываться!🧐');
+        marks.splice(0.5);
+    }
+    for(let i = 0; i < marks.length; i++){
+        sum += marks[i];
+        averageMark = sum/marks.length;
+    }
+    return averageMark;
 }
 
+
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let year = new Date().getFullYear()-18;
+    if (dateOfBirthday.getFullYear() < year) {
+        return(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    } else {
+        return(`Не желаете ли олд-фэшн, ${name} ?`);
+    } 
 }
