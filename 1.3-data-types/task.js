c47a99e45872a493423ca0a93aa74a6df1f9ed96
Murat Kurbanov <muratkurbanov@MacBook-Pro-Murat.local>
2021-02-28 15:58:37 +0300

    Работа

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,47 @@
+'use strict'
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    // contribution = 0; // первоначальный взнос
+    // amount = 50000; // сумма кредита
+    date = (new Date().getMonth()); // срок в месяцах
+    let s = amount - contribution; // сумма кредита - первоначальный взнос
+    let p = (percent/100)/12;
+
+    // let formAmount = s * (p + p / ((Math.pow(1 + p, date)) - 1));
+    let formAmount = s * (percent + percent / ((Math.pow(1 + p, date)) - 1));
+
+    
+    console.log(formAmount);
+
+    let = totalAmount = formAmount / 100;
+
+    return +totalAmount.toFixed(2);
 }
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//     // код для задачи №1 писать здесь
+//     let contribution = 0; // первоначальный взнос
+//     let amount = 50000; // сумма кредита
+//     let date = new Date().getMonth(); // срок в месяцах
+    
+//     let percent = 10; // процентной ставки
+//     let firstAmount = amount - contribution; // сумма кредита - первоначальный взнос
+
+//     let totalAmount = firstAmount * (percent + percent / (((1 + percent) Math.pow(date)) - 1));
+    
+//     console.log(totalAmount);
+
+//     return totalAmount;
+// }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    let name;
+    let noName = 'Аноним';
+    if (name == undefined || name == null || name == '') {
+        console.log(`Привет, мир! Меня зовут ${noName}`);
+    } else {
+        console.log(`Привет, мир! Меня зовут ${name}`);
+    }
+    //return getgreeting;
 }
